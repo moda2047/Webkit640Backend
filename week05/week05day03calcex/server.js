4a078d4edcaaa9f612fece4e37edec3f23db0868
moda <moda2047@naver.com>
2024-02-21 16:14:10 +0900

    REST Api로 사칙연산 server기능 완성 0221

--- week05/week05day03calcex/server.js
+++ week05/week05day03calcex/server.js
@@ -4,11 +4,72 @@ const app = express();
 const cors = require("cors");
 const router = express.Router();
 
-app.set("port", 3000);
+app.set("port", 5000);
 
-//사칙연산 기능
+// 사칙연산이 기능
+// REST api로 구현
+// 클라이언트에서 Ajax로 접속.
 
-const server = http.createServer();
+const calcUrl = "/calc/:a/:b";
+let a = 0;
+let b = 0;
+
+// 미들웨어에서 파라미터를 전역 변수에 저장
+app.use(calcUrl, (req, res, next) => {
+  console.log("미들웨어 호출!");
+  a = Number(req.params.a);
+  b = Number(req.params.b);
+  next();
+});
+
+router.route(calcUrl).get((req, res) => {
+  // 더하기
+  let resultData = {
+    result: a + b,
+    method: "get",
+    oper: "+",
+    date: new Date(),
+  };
+  console.log(resultData);
+  res.send(resultData);
+});
+router.route(calcUrl).delete((req, res) => {
+  // 빼기
+  let resultData = {
+    result: a - b,
+    method: "delete",
+    oper: "-",
+    date: new Date(),
+  };
+  console.log(resultData);
+  res.send(resultData);
+});
+router.route(calcUrl).post((req, res) => {
+  // 곱하기
+  let resultData = {
+    result: a * b,
+    method: "post",
+    oper: "*",
+    date: new Date(),
+  };
+  console.log(resultData);
+  res.send(resultData);
+});
+router.route(calcUrl).put((req, res) => {
+  // 나누기
+  let resultData = {
+    result: a / b,
+    method: "put",
+    oper: "/",
+    date: new Date(),
+  };
+  console.log(resultData);
+  res.send(resultData);
+});
+
+// 서버 실행 전에 라우터 미들웨어 등록
+app.use(router);
+const server = http.createServer(app);
 server.listen(app.get("port"), () => {
-  console.log("서버 실해중 httpL//localhost:" + app.get("port"));
+  console.log("서버 실행 중 ... http://localhost:" + app.get("port"));
 });
