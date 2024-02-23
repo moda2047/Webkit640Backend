ee08d95f90de1870ea45faa6a02e2900c72304de
moda <moda2047@naver.com>
2024-02-23 14:23:17 +0900

    몽고db 테스트 완료 0223

--- week05/week05day05mongodb/server.js
+++ week05/week05day05mongodb/server.js
@@ -1,9 +1,45 @@
 const MongoClient = require("mongodb").MongoClient;
 
 const dbUrl = "mongodb://localhost";
-MongoClient.connect(dbUrl, function (err, client) {
-  if (err) throw err;
+let db = null;
+function connectDb() {
+  MongoClient.connect(
+    dbUrl,
+    { useUnifiedTopology: true },
+    function (err, client) {
+      if (err) throw err;
 
-  let db = client.db("vehicle");
-  console.log(db);
+      db = client.db("vehicle");
+      console.log("DB 접속 성공!");
+    }
+  );
+}
+
+const express = require("express");
+const app = express();
+const http = require("http");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.get("/car", (req, res) => {
+  console.log("GET - /car");
+  res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
+  if (db) {
+    db.collection("car")
+      .find()
+      .toArray(function (err, carList) {
+        req.app.render("car_list", { carList }, (err, html) => {
+          res.end(html);
+        });
+      });
+  } else {
+    res.end("<h1>Error: DB가 없습니다!</h1>");
+  }
+});
+
+const server = http.createServer(app);
+server.listen(3000, () => {
+  console.log("서버 실행 중 => http://localhost:3000");
+  connectDb();
 });
